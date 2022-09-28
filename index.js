6b0ce9a33264a90399f46415778d3b95c0b401f1
Iracopter <114257500+Iracopter@users.noreply.github.com>
2022-09-28 13:37:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var x = 1;
+var y = 2;
+var res1 = x.toString()+y.toString(); // Допишіть код, необхідно використовувати змінні x та y
+console.log(res1); // "12"
+console.log(typeof res1); // "string"
+
+var res2 = true+y.toString(); // Допишіть код, необхідно використовувати змінні x та y
+console.log(res2); // "true2"
+console.log(typeof res2); // "string"
+
+var res3 =x<y; // Допишіть код, необхідно використовувати змінні x та y
+console.log(res3); // true
+console.log(typeof res3); // "boolean"
+
+var res4 = x*0/0; // Допишіть код, необхідно використовувати змінні x та y
+console.log(res4); // NaN
+console.log(typeof res4); // "number"
